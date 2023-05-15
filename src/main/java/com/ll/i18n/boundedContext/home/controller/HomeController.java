7d604eea3a4820fe246a7007a35bc266592ba2d8
seungyeoni <dnri33@naver.com>
2023-05-15 20:23:51 +0900

    messages

--- src/main/java/com/ll/i18n/boundedContext/home/controller/HomeController.java
+++ src/main/java/com/ll/i18n/boundedContext/home/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.ll.i18n;
+package com.ll.i18n.boundedContext.home.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
